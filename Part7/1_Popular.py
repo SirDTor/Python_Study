57a33ffd741472c940f9a2eb5ff3d8db23805842
zorindaniilleon <daniil.zorin.leon@gmail.com>
2023-12-10 20:08:07 +0700

    Part7
    Task1

---
+++ Part7/1_Popular.py
@@ -0,0 +1,35 @@
+from collections import OrderedDict
+
+chars_popularity = {}
+words_popularity = {}
+
+
+def FindPopularChars(origtext: str):
+    dataChars = OrderedDict()
+    for char in origtext:
+        if char != ' ':
+            if dataChars.__contains__(char):
+                dataChars[char] += 1
+            else:
+                dataChars[char] = 1
+            countChar = dataChars[char]
+            chars_popularity[char] = countChar
+
+
+def FindPopularWords(origtext: str):
+    dataWords = OrderedDict()
+    arrayOfWord = origtext.split(' ')
+    for word in arrayOfWord:
+        if dataWords.__contains__(word):
+            dataWords[word] += 1
+        else:
+            dataWords[word] = 1
+        countWords = dataWords[word]
+        words_popularity[word] = countWords
+
+
+text = "hello, word of word"
+FindPopularChars(text)
+FindPopularWords(text)
+print(chars_popularity)
+print(words_popularity)
